7a6ae3e3916d173e14a532d6176491cff4582220
Vivek Srivastava <vivek99srivastava@gmail.com>
2018-06-24 21:12:26 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+myData: Array<any>;
+
+constructor(private http:Http){
+ this.http.get('http://jsonplaceholder.typicode.com/photos').pipe(map(response => response.json())).subscribe(res => this.myData =res);
+ 
+}
+
+}
+
+
+
+
